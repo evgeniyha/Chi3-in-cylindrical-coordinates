7f37f895a9e3baf95f950f1545fcd7677d96afc0
Evgenii Menshikov <evgenii.menshikov@metalab.ifmo.ru>
2024-09-16 20:42:47 +0200

    optimized performance

--- tens_cyl.m
+++ tens_cyl.m
@@ -1,4 +1,5 @@
-clear all
+clear 
+clc
 % Define symbolic variables
 syms phi;
 % Define transformation matrix R
@@ -46,21 +47,37 @@ chi3(2,1,1,2) = chi4_value4;
 % Assuming the rest of the components are set to zero
 % Transform chi to cylindrical coordinates (rho, phi, z)
 chi3_cyl = sym(zeros(3,3,3,3));
+% for n1 = 1:3
+%     for n2 = 1:3
+%         for n3 = 1:3
+%             for n4 = 1:3
+%                 sum = sym(0);
+%                 for i1 = 1:3
+%                     for i2 = 1:3
+%                         for i3 = 1:3
+%                             for i4 = 1:3
+%                                 sum = sum + R(n1,i1)*R(n2,i2)*R(n3,i3)*R(n4,i4)*chi3(i1,i2,i3,i4);
+%                             end
+%                         end
+%                     end
+%                 end
+%                 chi3_cyl(n1,n2,n3,n4) = simplify(sum);
+%             end
+%         end
+%     end
+% end
+
 for n1 = 1:3
     for n2 = 1:3
         for n3 = 1:3
             for n4 = 1:3
-                sum = sym(0);
-                for i1 = 1:3
-                    for i2 = 1:3
-                        for i3 = 1:3
-                            for i4 = 1:3
-                                sum = sum + R(n1,i1)*R(n2,i2)*R(n3,i3)*R(n4,i4)*chi3(i1,i2,i3,i4);
-                            end
-                        end
-                    end
-                end
-                chi3_cyl(n1,n2,n3,n4) = simplify(sum);
+
+                Rm = reshape(R(n1,:), [3,1])*R(n2,:);
+                Rd = reshape(Rm(:)*R(n3,:),[3, 3, 3]);
+                Rdm = reshape(Rd(:)*R(n4,:),[3, 3, 3, 3]);
+                
+                chi3_cyl(n1,n2,n3,n4) = simplify(sum(Rdm(:,:,:,:).*chi3(:,:,:,:),'all'));
+                
             end
         end
     end
